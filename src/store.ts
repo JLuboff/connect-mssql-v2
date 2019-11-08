8ac9c7edf7780bd8764284f59fb4fe525722c635
Jason L <jluboff@gmail.com>
2019-11-07 16:38:19 -0800

    Created constructor and options interface

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,26 @@
-import sql from 'mssql';
+import sql, { config as SQLConfig } from 'mssql';
 
-class MSSQLStore {}
+interface StoreOptions {
+  table?: string;
+  ttl?: number;
+  autoRemove?: boolean;
+  autoRemoveInterval?: number;
+  autoRemoveCallback?: any;
+}
+
+module.exports = (session: any) => {
+  const Store = session.Store || session.session.Store;
+  class MSSQLStore extends Store {
+    constructor(config: SQLConfig, options: StoreOptions) {
+      super();
+      this.table = options.table || 'sessions';
+      this.ttl = options.ttl || 1000 * 60 * 60 * 24;
+      this.autoRemove = options.autoRemove || false;
+      this.autoRemoveInterval = options.autoRemoveInterval || 1000 * 60 * 10;
+      this.config = config;
+    }
+
+    databaseConnection = new sql.ConnectionPool(this.config);
+    
+  }
+};
